a8e6d611db3e4fa3ab21e73a7bf48c386076c48c
ayfengl <37704969+ayfengl@users.noreply.github.com>
2023-06-28 14:01:29 -0700

    Update iframe.js

--- blocks/iframe/iframe.js
+++ blocks/iframe/iframe.js
@@ -1,1 +1,7 @@
+<iframe id="inlineFrameExample"
+    title="Inline Frame Example"
+    width="800"
+    height="800"
+    src="https://calendar.google.com/calendar/u/0/embed?mode=AGENDA&height=600&wkst=1&bgcolor=%23ff9900&src=le40uvlqte5b3uqg1b2v1h8af4@group.calendar.google.com&color=%23AB8B00&ctz=America/Los_Angeles">
+</iframe>
 
