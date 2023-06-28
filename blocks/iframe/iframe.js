74657f4fe3eca9f71ca000d716b17e16c41f05c2
ayfengl <37704969+ayfengl@users.noreply.github.com>
2023-06-28 14:12:10 -0700

    Update iframe.js

--- blocks/iframe/iframe.js
+++ blocks/iframe/iframe.js
@@ -1,3 +1,6 @@
-<iframe frameborder="0" height="340" id="sideCalendar" scrolling="no" src="https://www.google.com/calendar/embed?mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23ff9900&amp;src=le40uvlqte5b3uqg1b2v1h8af4%40group.calendar.google.com&amp;color=%23AB8B00&amp;ctz=America%2FLos_Angeles" style="border-width: 0;" width="100%">
-</iframe>
+import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
+export default function decorate(block) {
+  <iframe frameborder="0" height="340" id="sideCalendar" scrolling="no" src="https://www.google.com/calendar/embed?mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23ff9900&amp;src=le40uvlqte5b3uqg1b2v1h8af4%40group.calendar.google.com&amp;color=%23AB8B00&amp;ctz=America%2FLos_Angeles" style="border-width: 0;" width="100%">
+</iframe>
+}
